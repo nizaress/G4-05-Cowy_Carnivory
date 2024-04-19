ee56f749622116ca63586aee33d93a0a72daae7f
Nur del Amo García <ndag1@alu.ua.es>
2024-04-19 13:27:09 +0200

    update

--- DSS-First-Delivery/Work-Project/storage/framework/views/f9d0ba1a64f04cc582bb03b61841dc6d.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\nur29\OneDrive\Escritorio\Laravel\G4-05-Cowy_Carnivory\DSS-First-Delivery\Work-Project\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
