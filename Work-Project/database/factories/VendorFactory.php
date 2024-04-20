5584b455b64771fbf5dbb00108308da71be7e369
cacs2-ua <cacs2@alu.ua.es>
2024-04-20 19:14:23 +0200

    Hash Factory added to vendor

---
+++ Work-Project/database/factories/VendorFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Vendor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vendor>
+ */
+class VendorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Vendor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $this->faker->company(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'accountNumber' => $this->faker->bankAccountNumber(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
