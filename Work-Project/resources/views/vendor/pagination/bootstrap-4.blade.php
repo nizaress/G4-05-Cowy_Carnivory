1971e518c09a75c88d946a374e26a528502a43e3
cacs2-ua <cacs2@alu.ua.es>
2024-04-20 16:51:43 +0200

    Pagination Done

--- Work-Project/resources/views/vendor/pagination/bootstrap-4.blade.php
+++ Work-Project/resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -3,27 +3,30 @@
         <ul class="pagination justify-content-center">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                    <span class="page-link page-item disabled" style="background-color: #e9ecef; color: #6c757d;">&lsaquo;</span>
-                    <br>
+                <span class="page-link page-item disabled" style="background-color: #e9ecef; color: #6c757d; font-size: 20px; padding: 3px; border-radius: 50%; border: 2px solid #4CAF50;">&lsaquo;</span>
+                <br>
+                <br>
+                
             @else
-                    <a class="page-link page-item" href="{{ $paginator->previousPageUrl() }}" style="background-color: #4CAF50; color: white;" rel="prev">&lsaquo;</a>
-                    <br>
+                <a class="page-link page-item" href="{{ $paginator->previousPageUrl() }}" style="background-color: #4CAF50; color: white; font-size: 20px; padding: 3px; border-radius: 50%; border: 2px solid #45a0aa;" rel="prev">&lsaquo;</a>
+                <br>
+                <br>
             @endif
 
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled"><span class="page-link" style="background-color: #4CAF50; color: white;">{{ $element }}</span></li>
+                    <li class="page-item disabled"><span class="page-link" style="background-color: #4CAF50; color: white; font-size: 20px;">{{ $element }}</span></li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <span class="page-item active" style="background-color: #4CAF50;"><span class="page-link" style="background-color: #45a0aa; color: white; border-color: #4CAF50;">{{ $page }}</span></span>
+                            <span class="page-item active" style="background-color: #4CAF50; font-size: 20px; padding: 5px; border-radius: 50%; border: 2px solid #45a0aa;"><span class="page-link" style="background-color: #45a0aa; color: white; border-color: #4CAF50; border-radius: 50%; padding: 5px;">{{ $page }}</span></span>
                         @else
-                            <span class="page-item"><a class="page-link" href="{{ $url }}" style="background-color: white; color: #4CAF50;">{{ $page }}</a></span>
+                            <span class="page-item"><a class="page-link" href="{{ $url }}" style="background-color: white; color: #4CAF50; font-size: 20px; padding: 5px; border-radius: 50%; border: 2px solid #4CAF50;">{{ $page }}</a></span>
                         @endif
                     @endforeach
                 @endif
@@ -32,11 +35,13 @@
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <br>
-                <a class="page-link page-item" href="{{ $paginator->nextPageUrl() }}" style="background-color: #4CAF50; color: white;" rel="next">&rsaquo;</a>
+                <br>
+                <a class="page-link page-item" href="{{ $paginator->nextPageUrl() }}" style="background-color: #4CAF50; color: white; font-size: 20px; padding: 3px; border-radius: 50%; border: 2px solid #45a0aa;" rel="next">&rsaquo;</a>
                 
             @else
                 <br>
-                <span class="page-link page-item disabled" style="background-color: #e9ecef; color: #6c757d;">&rsaquo;</span>
+                <br>
+                <span class="page-link page-item disabled" style="background-color: #e9ecef; color: #6c757d; font-size: 20px; padding: 3px; border-radius: 50%; border: 2px solid #4CAF50;">&rsaquo;</span>
             @endif
         </ul>
     </nav>
