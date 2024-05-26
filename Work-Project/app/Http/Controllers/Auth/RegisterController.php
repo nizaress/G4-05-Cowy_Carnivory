789a07eb3d8ff5e2b2eee3c798dd50f2bc4d5606
cacs2-ua <cacs2@alu.ua.es>
2024-05-26 02:28:32 +0200

    Little change to Register Controller

--- Work-Project/app/Http/Controllers/Auth/RegisterController.php
+++ Work-Project/app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:5', 'confirmed'],
             'address' => ['required', 'string', 'max:255'],
             'phone_number' => ['required', 'numeric'],
             'card_number' => ['required', 'numeric', 'digits:16'],
